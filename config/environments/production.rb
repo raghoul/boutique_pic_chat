affd84721be2a049b96e86eb9b97d334d72edbf9
Raghoul_jeminthar <raghoul18@gmail.com>
2021-06-10 16:35:50 +0200

    gemfile mise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,13 +61,9 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "cat_store_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "EventBrite_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-
-  ### ===> Ligne à changer quand on passe en master sur Heroku
-  config.action_mailer.default_url_options = { :host => 'https://cat-store-thp.herokuapp.com/' }
-  ### 
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
